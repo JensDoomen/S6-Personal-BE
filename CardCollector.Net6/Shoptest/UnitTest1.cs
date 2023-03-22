546cc674634bd5506ef941ffd9c99de88b220f09
Jens Doomen <457543@student.fontys.nl>
2023-03-22 11:36:47 +0100

    ..

---
+++ CardCollector.Net6/Shoptest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Shoptest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
