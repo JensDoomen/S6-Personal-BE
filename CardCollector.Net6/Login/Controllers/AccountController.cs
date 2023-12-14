2ed8a167e7c2fe289dc0b3f5352131b15804119e
Jens Doomen <457543@student.fontys.nl>
2023-12-14 12:49:33 +0100

    login

---
+++ CardCollector.Net6/Login/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace Login.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        
+        public AccountController(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            var result = await _signInManager.PasswordSignInAsync(username, password, false, false);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+    }
+}
